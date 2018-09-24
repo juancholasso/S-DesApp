afe8cb49f50551f30a02db1fc0954d73ed0125f3
juancholasso <juancholasso@hotmail.com>
2018-09-23 21:28:48 -0500

    Nueva clase Cipher

---
+++ src/com/unbosque/models/Cipher.java
@@ -0,0 +1,13 @@
+package com.unbosque.models;
+/**
+ * 
+ * @author Juan Pablo Camargo Lasso (juancholasso)
+ * @author Samuel Delgado Muñoz
+ *
+ */
+/*
+ * S-Des Cipher
+ */
+public class Cipher {
+
+}
